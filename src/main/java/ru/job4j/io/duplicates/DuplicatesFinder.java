2cb12fc4b9bb747ae2e0aef8f4f708b3900c6fab
TujhZreitd <egor.yakushev.96@bk.ru>
2024-05-03 11:04:49 +0300

    11. Поиск дубликатов [#315066 #494149]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,10 +3,18 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor res = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("C:\\Users\\pc\\Desktop\\X1"), res);
+        for (Map.Entry<FileProperty, List<Path>> entry : res.getFiles().entrySet()) {
+            List<Path> result = entry.getValue();
+            if (result.size() > 1) {
+                System.out.println(result);
+            }
+        }
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -10,18 +10,20 @@ import java.util.*;
 import static java.nio.file.Files.size;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Map<String, Long> files = new HashMap<>();
+    private Map<FileProperty, List<Path>> files = new HashMap<>();
+
+    public Map<FileProperty, List<Path>> getFiles() {
+        return files;
+    }
 
     @Override
     public FileVisitResult visitFile(Path file,
                                      BasicFileAttributes attributes) throws IOException {
         FileProperty newFile = new FileProperty(size(file), file.toFile().getName());
-        if (files.containsKey(newFile.getName())) {
-            if (files.get(newFile.getName()) == newFile.getSize()) {
-                System.out.println(file);
-            }
+        if (!files.containsKey(newFile)) {
+            files.put(newFile, new ArrayList<>(List.of(file)));
         } else {
-            files.put(newFile.getName(), newFile.getSize());
+            files.get(newFile).add(file);
         }
         return super.visitFile(file, attributes);
     }
