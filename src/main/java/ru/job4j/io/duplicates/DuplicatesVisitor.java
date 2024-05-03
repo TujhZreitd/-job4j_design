e839449238b90c3068706edd40bf9b9aab42bef9
TujhZreitd <egor.yakushev.96@bk.ru>
2024-05-03 14:01:18 +0300

    11. Поиск дубликатов [#315066 #494149]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -20,12 +20,7 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFile(Path file,
                                      BasicFileAttributes attributes) throws IOException {
         FileProperty newFile = new FileProperty(size(file), file.toFile().getName());
-        if (!files.containsKey(newFile)) {
-            files.computeIfAbsent(newFile, s -> new ArrayList<>(List.of(file)));
-        } else {
-            files.get(newFile).add(file);
-        }
-
+        files.computeIfAbsent(newFile, k -> new ArrayList<Path>()).add(file);
         return super.visitFile(file, attributes);
     }
 }
