3b4ec38153f6ed33afd9112a25b1bb4be20c78a9
TujhZreitd <egor.yakushev.96@bk.ru>
2024-05-16 10:38:48 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,16 +5,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream output = socket.getOutputStream();
+                try (BufferedWriter output = new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream()));
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    if (input.readLine().contains("msg=Bye")) {
+                    output.write("HTTP/1.1 200 OK\r\n\r\n");
+                    String firstString = input.readLine();
+                    String[] msg = firstString
+                            .replace("GET /?", "")
+                            .replace(" HTTP/1.1", "")
+                            .split("=");
+                    if ("Exit".equals(msg[1])) {
                         server.close();
+                    } else {
+                        output.write(msg[1]);
                     }
                     output.flush();
                 }
