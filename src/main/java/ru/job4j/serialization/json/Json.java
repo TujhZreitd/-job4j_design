062c3c9647546e5fccc738d0039084ee6af4eff9
TujhZreitd <egor.yakushev.96@bk.ru>
2024-05-16 11:30:12 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Json.java
@@ -0,0 +1,67 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
+public class Json {
+    public static void main(String[] args) {
+        Employee employeeFirst = new Employee(true,
+                35,
+                "Anton",
+                new Car("Nissan", "red"),
+                new String[]{"Worker", "Married"});
+        Gson gson = new GsonBuilder().create();
+        String employeeGson = gson.toJson(employeeFirst);
+        System.out.println(employeeGson);
+        Employee employeeSecond = gson.fromJson(employeeGson, Employee.class);
+        System.out.println(employeeSecond);
+    }
+}
+
+class Employee {
+    private final boolean bestEmployee;
+    private final int age;
+    private final String name;
+    private final Car car;
+    private final String[] status;
+
+    public Employee(boolean bestEmployee, int age, String name, Car car, String[] status) {
+        this.bestEmployee = bestEmployee;
+        this.age = age;
+        this.name = name;
+        this.car = car;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "bestEmployee=" + bestEmployee
+                + ", age=" + age
+                + ", name='" + name
+                + '\''
+                + ", car=" + car
+                + ", status=" + Arrays.toString(status)
+                + '}';
+    }
+}
+
+class Car {
+    private final String model;
+    private final String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "model='" + model
+                + '\''
+                + ", color='" + color
+                + '\''
+                + '}';
+    }
+}
