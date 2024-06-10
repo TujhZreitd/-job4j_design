9b8d3ac90493924bb645bb9f404301bdb632f3f6
TujhZreitd <egor.yakushev.96@bk.ru>
2024-06-10 13:32:09 +0300

    0.1. Statement [#379306]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,117 @@
+package ru.job4j.jdbc;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws Exception {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws Exception {
+        Class.forName(properties.getProperty("driver_class"));
+        connection = DriverManager.getConnection(
+                properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password"));
+    }
+
+    private void executeAction(String sql) throws Exception {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+    }
+
+    public void createTable(String tableName) throws Exception {
+        String sql = String.format(
+                "CREATE TABLE IF NOT EXISTS %s();",
+                tableName
+        );
+        executeAction(sql);
+    }
+
+    public void dropTable(String tableName) throws Exception {
+        String sql = String.format(
+            "DROP TABLE IF EXISTS %s;",
+            tableName
+        );
+        executeAction(sql);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) throws Exception {
+        String sql = String.format(
+            "ALTER TABLE %s ADD COLUMN %s %s;",
+            tableName,
+            columnName,
+            type
+        );
+        executeAction(sql);
+    }
+
+    public void dropColumn(String tableName, String columnName) throws Exception {
+        String sql = String.format(
+            "ALTER TABLE %s DROP %s;",
+            tableName,
+            columnName
+        );
+        executeAction(sql);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws Exception {
+        String sql = String.format(
+            "ALTER TABLE %s RENAME COLUMN %s TO %s;",
+            tableName,
+            columnName,
+            newColumnName
+        );
+        executeAction(sql);
+    }
+
+    public String getTableScheme(String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("app.properties")) {
+            properties.load(in);
+        }
+        TableEditor tableEditor = new TableEditor(properties);
+        tableEditor.createTable("products");
+        tableEditor.addColumn("products", "name", "text");
+        tableEditor.renameColumn("products", "name", "nameProduct");
+        tableEditor.dropColumn("products", "nameProduct");
+        System.out.println(tableEditor.getTableScheme("products"));
+        tableEditor.dropTable("products");
+    }
+}
